c16873c08c4141ed031710225b58d8ac8ccd78a6
minal <salviminal21@gmail.com>
2022-11-02 20:15:27 +0530

    0.2

---
+++ src/app/table/table-datasource.ts
@@ -0,0 +1,98 @@
+import { DataSource } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { map } from 'rxjs/operators';
+import { Observable, of as observableOf, merge } from 'rxjs';
+
+// TODO: Replace this with your own data model type
+export interface TableItem {
+  name: string;
+  id: number;
+}
+
+// TODO: replace this with real data from your application
+const EXAMPLE_DATA: TableItem[] = [
+  {id: 1, name: '	Ph.D. in Arts (Economics) from Center for Advanced Studies in Economics, Department of economics (Know as Mumbai School of Economics & Public Finance), University of Mumbai in year 2011. '},
+  {id: 2, name: '	Masters in Economics (M.A. – Economics) from Center of Advanced Studies in Economics, Department of Economics, University of Mumbai, October 1994. '},
+  {id: 3, name: '	Masters in Political Science (M.A. – Politics) from Department of Civics & Politics, University of Mumbai, November 1996.'},
+  {id: 4, name: '	Bachelors in Economics (B.A. – Economics) from Vidyawardhini College (Affiliated to University of Poona), May 1992.'},
+  {id: 5, name: '	Bachelor of Education (B.Ed) Gangamai College of Education, North Maharashtra University, May 1999.'},
+  {id: 6, name: '	National Eligibility Test (NET – in Economics) by University Grant Commission (UGC – New Delhi) passed in year 2000. '},
+  
+];
+
+/**
+ * Data source for the Table view. This class should
+ * encapsulate all logic for fetching and manipulating the displayed data
+ * (including sorting, pagination, and filtering).
+ */
+export class TableDataSource extends DataSource<TableItem> {
+  data: TableItem[] = EXAMPLE_DATA;
+  paginator: MatPaginator | undefined;
+  sort: MatSort | undefined;
+
+  constructor() {
+    super();
+  }
+
+  /**
+   * Connect this data source to the table. The table will only update when
+   * the returned stream emits new items.
+   * @returns A stream of the items to be rendered.
+   */
+  connect(): Observable<TableItem[]> {
+    if (this.paginator && this.sort) {
+      // Combine everything that affects the rendered data into one update
+      // stream for the data-table to consume.
+      return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange)
+        .pipe(map(() => {
+          return this.getPagedData(this.getSortedData([...this.data ]));
+        }));
+    } else {
+      throw Error('Please set the paginator and sort on the data source before connecting.');
+    }
+  }
+
+  /**
+   *  Called when the table is being destroyed. Use this function, to clean up
+   * any open connections or free any held resources that were set up during connect.
+   */
+  disconnect(): void {}
+
+  /**
+   * Paginate the data (client-side). If you're using server-side pagination,
+   * this would be replaced by requesting the appropriate data from the server.
+   */
+  private getPagedData(data: TableItem[]): TableItem[] {
+    if (this.paginator) {
+      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+      return data.splice(startIndex, this.paginator.pageSize);
+    } else {
+      return data;
+    }
+  }
+
+  /**
+   * Sort the data (client-side). If you're using server-side sorting,
+   * this would be replaced by requesting the appropriate data from the server.
+   */
+  private getSortedData(data: TableItem[]): TableItem[] {
+    if (!this.sort || !this.sort.active || this.sort.direction === '') {
+      return data;
+    }
+
+    return data.sort((a, b) => {
+      const isAsc = this.sort?.direction === 'asc';
+      switch (this.sort?.active) {
+        case 'name': return compare(a.name, b.name, isAsc);
+        case 'id': return compare(+a.id, +b.id, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
+function compare(a: string | number, b: string | number, isAsc: boolean): number {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
